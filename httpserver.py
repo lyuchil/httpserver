1928fd7f36602802e0fa649b88d01556868d7fd1
lyuchil <852714@gmail.com>
2022-04-10 21:33:58 -0400

    comment + formatting

--- httpserver.py
+++ httpserver.py
@@ -7,13 +7,15 @@ errmsg = 'HTTP/1.1 404 NOT FOUND\r\n\r\n'
 response10 = 'HTTP/1.0 200 OK\r\n\r\n'
 response11 = 'HTTP/1.1 200 OK\r\n\r\n'
 
-
-def working_for_client(client_server, client_addr, timeout_var, index):
+# function that handles individual request
+def working_for_client(client_server, client_addr, timeout_var):
     print(f"Thread {client_addr} created \n")
-    client_server.settimeout(timeout_var)
 
+    # setting the timeout variable based on the command line
+    client_server.settimeout(timeout_var)
     header_1 = ""
 
+    # catching timeout error when there is no data coming through
     try:
         HTTP_request = client_server.recv(1024).decode()
     except TimeoutError:
@@ -23,20 +25,15 @@ def working_for_client(client_server, client_addr, timeout_var, index):
         client_server.send(response_1)
         client_server.close()
         sys.exit(1)
-    except Exception:
-        header_1 = errmsg
-        print(f"Thread {client_addr}: Error: unexpected end of input \n")
-        response_1 = header_1.encode()
-        client_server.send(response_1)
-        client_server.close()
-        sys.exit(1)
 
+    # parsing the HTTP request and extracting the file name and HTTP version
     parse = HTTP_request.split(" ")
     file_name_temp = parse[1]
     file_name = file_name_temp.lstrip("/")
     parse2 = parse[2].split('\r\n')
     HTTP_version = parse2[0]
 
+    # checking for the custom header that puts additional wait on the request
     if 'X-Additional-wait' in HTTP_request:
         custom_parse = HTTP_request.split()
         sleep_index = custom_parse.index('X-Additional-wait:')
@@ -44,6 +41,7 @@ def working_for_client(client_server, client_addr, timeout_var, index):
         print(f"Thread {client_addr}: Additional wait: {sleep_time} \n")
         time.sleep(sleep_time)
 
+    # checking for the correct request type sent by the HTTP request
     if parse[0] != "GET":
         header_1 = errmsg
         print(f"Thread {client_addr}: Error: invalid request line \n")
@@ -52,6 +50,7 @@ def working_for_client(client_server, client_addr, timeout_var, index):
         client_server.close()
         sys.exit(1)
 
+    # checking for the correct HTTP version and setting the correct header
     try:
         if HTTP_version == "HTTP/1.1":
             header_1 = response11
@@ -63,6 +62,7 @@ def working_for_client(client_server, client_addr, timeout_var, index):
         response_1 = header_1.encode()
         client_server.send(response_1)
         client_server.close()
+        sys.exit(1)
 
     # trying to retrieve file
     if file_name == '':
@@ -78,15 +78,15 @@ def working_for_client(client_server, client_addr, timeout_var, index):
         client_server.send(response_1)
         print(f"Thread {client_addr}: Success: served file {file_name} \n")
         client_server.close()
-
-
-
+        sys.exit(1)
+    # sending 404 Error when the file name does not exist
     except FileNotFoundError:
         header_1 = errmsg
         print(f'Thread {client_addr}: Error: invalid path \n')
         response_1 = header_1.encode()
         client_server.send(response_1)
         client_server.close()
+        sys.exit(1)
 
 
 # ----- main ------
@@ -119,12 +119,11 @@ server_socket.listen(max_rq)
 
 total_threads = []
 
-count = 0
-
 while True:
 
     client_server, client_addr = server_socket.accept()
 
+    # checking for maximum request by the number of threads
     if len(total_threads) == max_rq:
         print("Error too many connections")
         header = errmsg
@@ -132,18 +131,18 @@ while True:
         client_server.send(response)
         client_server.close()
     else:
-        count += 1
+
         print(f'Information: received new connection from {client_addr}, port {port}')
 
-        t = threading.Thread(target=working_for_client,
-                             args=(client_server, client_addr, timeout, count))
+        # creating threads when receiving request
+        t = threading.Thread(target=working_for_client, args=(client_server, client_addr, timeout))
         t.start()
         total_threads.append(t)
 
-        t.join()
+        # making sure that when the thread ends, it is removed from the list of threads and open up spot for new requests
         for threads in total_threads:
             if not threads.is_alive():
                 total_threads.remove(threads)
-                count -= 1
+
 
 
